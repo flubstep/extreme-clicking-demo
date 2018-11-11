c5a3dae43d60ef61974c6c4b24a8cb3262352cc0
Albert Sheu <albert.sheu@gmail.com>
2018-11-11 10:23:53 -0500

    Add warning about mobile web

--- src/App.js
+++ src/App.js
@@ -28,10 +28,15 @@ class App extends Component {
       <div className="App">
         <div>
           <DismissableNotification message="Got it!">
-            Extreme clicking is a method of bounding box annotation that is done by having the annotator
-            click on four points to label a rectangle: the topmost, leftmost, rightmost, and bottommost
-            point of every object to be labeled. Try picking out a piece of fruit and clicking on the top,
-            left, bottom, and right points to draw a bounding box around it.
+            <div>
+              <i>BTW: Doesn't work too well on mobile web yet.</i>
+            </div>
+            <div className="section">
+              Extreme clicking is a method of bounding box annotation that is done by having the annotator
+              click on four points to label a rectangle: the topmost, leftmost, rightmost, and bottommost
+              point of every object to be labeled. Try picking out a piece of fruit and clicking on the top,
+              left, bottom, and right points to draw a bounding box around it.
+            </div>
           </DismissableNotification>
           <ExtremeClickingImageLabeler
             height={this.state.height}
